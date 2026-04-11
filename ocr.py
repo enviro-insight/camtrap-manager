fa5dd81e660d6bd9fcc7dc35b8a6a5549948c155
Ian Engelbrecht <ianicus.za@gmail.com>
2026-04-11 11:35:38 +0200

    reading whole dir

--- ocr.py
+++ ocr.py
@@ -17,34 +17,31 @@ from tkinter.filedialog import askdirectory
 Tk().withdraw()
 
 folder_path = askdirectory(title="Select folder containing video files")
-
-
-print("Reading camera ID from first video")
-first_video = next(Path(folder_path).glob("*.mp4"), None)
-if not first_video:
+videos = list(Path(folder_path).glob("*.mp4"))
+if not videos:
     print("Error: No video files found in the selected directory.")
     exit(1)
-else:
-    print("First video file found:", first_video)
 
-cap = cv2.VideoCapture(str(first_video))
-cap.set(cv2.CAP_PROP_POS_FRAMES, 100)
-ret, frame = cap.read()
-cap.release()
+ocrs = set()
+start_time = time()
 
-# crop to metadata
-h, w = frame.shape[:2]
+for video in videos:
 
-# Example: bottom-right corner (tweak these numbers)
-roi = frame[int(h*0.9):h, int(w*0.4):w]
-cv2.imshow("ROI", roi)
-cv2.waitKey(0)
+    cap = cv2.VideoCapture(str(video))
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 100)
+    ret, frame = cap.read()
+    cap.release()
 
-if ret:
-    ocrs = set()
-    start_time = time()
-    for i in range(1, 100):
-    
+    # crop to metadata
+    h, w = frame.shape[:2]
+
+    # Example: bottom-right corner (tweak these numbers)
+    roi = frame[int(h*0.9):h, int(w*0.4):w]
+    # cv2.imshow("ROI", roi)
+    # cv2.waitKey(0)
+
+    if ret:
+
         text = pytesseract.image_to_string(roi)
         text = text.replace("CO", "C0")  # remove spaces
         match = re.search(r"C\d{3}", text)
@@ -52,15 +49,15 @@ if ret:
             camera_id = match.group(0)
             ocrs.add(camera_id)
         else:
-            print("Error: Could not extract camera ID from video on iteration", i)
+            print("Error: Could not extract camera ID from video on iteration", i)        
 
-    end_time = time()
-    # print pretty time H:M:S
-    elapsed_time = end_time - start_time
-    hours, rem = divmod(elapsed_time, 3600)
-    minutes, seconds = divmod(rem, 60)
-    print("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
-    print("OCR values: ", "|".join(list(ocrs)))
+    else:
+        print("Oops, we couldn't process the image!")
 
-else:
-    print("Oops, we couldn't process the image!")
+end_time = time()
+# print pretty time H:M:S
+elapsed_time = end_time - start_time
+hours, rem = divmod(elapsed_time, 3600)
+minutes, seconds = divmod(rem, 60)
+print("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+print("OCR values: ", "|".join(list(ocrs)))
