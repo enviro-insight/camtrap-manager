410d113552f9f548f5caae28d98dc51b2192ece7
Ian Engelbrecht <ianicus.za@gmail.com>
2026-04-11 12:45:28 +0200

    trying againg

--- metadata.py
+++ metadata.py
@@ -111,7 +111,7 @@ def main():
 
     args = parse_args()
 
-    if not args.input:
+    if 'input' not in args or not args.input:
 
         Tk.withdraw()
         directory = askdirectory(title="Select folder containing video files")
