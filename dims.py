d03b5994f0f36ba6fd1585f7317be96dbd5be835
Ian Engelbrecht <ianicus.za@gmail.com>
2026-04-21 20:58:36 +0200

    narrower dims

--- dims.py
+++ dims.py
@@ -1,7 +1,8 @@
 dims = {
     "spartan": {
-        "widthStart": 0.5,
-        "widthEnd": 0.6,
-        "height": 0.9
+        "widthStart": 0.52,
+        "widthEnd": 0.58,
+        "heightStart": 0.92,
+        "heightEnd": 0.98
     },
 }
